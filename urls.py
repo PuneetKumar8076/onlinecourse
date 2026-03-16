61df1500de5b5c18a71aa57ef97a529e73817724
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-17 00:17:20 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,12 +3,11 @@ from . import views
 
 urlpatterns = [
 
-    path('course/<int:course_id>/submit',
+    path('course/<int:course_id>/submit/',
          views.submit,
          name='submit'),
 
-    path('course/<int:course_id>/submission/<int:submission_id>/result',
+    path('course/<int:course_id>/submission/<int:submission_id>/result/',
          views.show_exam_result,
          name='show_exam_result'),
-
 ]
