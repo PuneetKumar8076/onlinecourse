fb80dbd6358c5022ca59e251849c37287a438c77
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-17 00:18:39 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,48 +1,20 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Course, Question, Choice, Submission
-
-
-def submit(request, course_id):
-
-    course = get_object_or_404(Course, pk=course_id)
-
-    if request.method == "POST":
-
-        selected_choices = request.POST.getlist('choice')
-
-        submission = Submission.objects.create()
-
-        for choice_id in selected_choices:
-            choice = Choice.objects.get(pk=choice_id)
-            submission.choices.add(choice)
-
-        return redirect(
-            'onlinecourse:show_exam_result',
-            course_id=course.id,
-            submission_id=submission.id
-        )
-
-
 def show_exam_result(request, course_id, submission_id):
-
-    course = get_object_or_404(Course, pk=course_id)
 
     submission = get_object_or_404(Submission, pk=submission_id)
 
     choices = submission.choices.all()
 
-    score = 0
     total = choices.count()
+    score = 0
 
     for choice in choices:
         if choice.is_correct:
             score += 1
 
     context = {
-        'course': course,
-        'choices': choices,
         'score': score,
-        'total': total
+        'total': total,
+        'choices': choices
     }
 
     return render(request, 'exam_result_bootstrap.html', context)
